5dbe42a05813f0edbf47ae4d5375bce89d26ddc1
Roman Batygin <r.batygin@intabia.ru>
2018-03-28 15:25:29 +0700

    some fixes

--- eca-client/src/main/java/eca/client/dictionary/ExperimentTypeDictionary.java
+++ eca-client/src/main/java/eca/client/dictionary/ExperimentTypeDictionary.java
@@ -8,8 +8,8 @@ package eca.client.dictionary;
 public class ExperimentTypeDictionary {
 
     public static final String NEURAL_NETWORK_NAME = "Нейронные сети";
-    public static final String HETEROGENEOUS_ENSEMBLE_NAME = "Неоднородный ансамблевый алгоритм";
-    public static final String MODIFIED_HETEROGENEOUS_ENSEMBLE_NAME = "Мод. неоднородный ансамблевый алгоритм";
+    public static final String HETEROGENEOUS_ENSEMBLE_NAME = "Неоднородный ансамбль";
+    public static final String MODIFIED_HETEROGENEOUS_ENSEMBLE_NAME = "Мод. неоднородный ансамбль";
     public static final String ADA_BOOST_NAME = "Алгоритм AdaBoost";
     public static final String STACKING_NAME = "Алгоритм Stacking";
     public static final String STACKING_CV_NAME = "Алгоритм Stacking CV";
