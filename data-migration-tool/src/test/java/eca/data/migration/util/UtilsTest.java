2b416e44caa849f7654e6fb6bc5179f186d4d28e
Roman Batygin <r.batygin@intabia.ru>
2018-04-19 11:23:25 +0700

    added testFormatValue test

--- data-migration-tool/src/test/java/eca/data/migration/util/UtilsTest.java
+++ data-migration-tool/src/test/java/eca/data/migration/util/UtilsTest.java
@@ -4,8 +4,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Unit tests for checking {@link Utils} functionality.
@@ -15,6 +20,7 @@ import java.util.Arrays;
 public class UtilsTest {
 
     private static final int VARCHAR_LENGTH = 255;
+    private static final String STRING_VALUE_FORMAT = "'%s'";
     private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private static final String COLUMN_FORMAT = "%s %s";
     private static final String NUMERIC_TYPE = "NUMERIC";
@@ -66,5 +72,38 @@ public class UtilsTest {
         attribute = new Attribute("dateAttribute", DATE_FORMAT);
         Assertions.assertThat(Utils.formatAttribute(attribute, COLUMN_FORMAT)).isEqualTo(
                 String.format(COLUMN_FORMAT, attribute.name(), TIMESTAMP_FORMAT));
+    }
+
+    /**
+     * Tests format value to column value in insert command.
+     * Case 1: Tests numeric attribute
+     * Case 2: Tests date attribute
+     * Case 3: Tests nominal attribute
+     */
+    @Test
+    public void testFormatValue() {
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        Attribute numericAttribute = new Attribute("numericAttribute");
+        attributes.add(numericAttribute);
+        Attribute dateAttribute = new Attribute("dateAttribute", DATE_FORMAT);
+        attributes.add(dateAttribute);
+        Attribute nominalAttribute = new Attribute("nominalAttribute", Arrays.asList("A", "B", "C"));
+        attributes.add(nominalAttribute);
+        Instances instances = new Instances("Relation", attributes, 10);
+        instances.setClassIndex(instances.numAttributes() - 1);
+        Instance instance = new DenseInstance(instances.numAttributes());
+        instance.setValue(numericAttribute, 56);
+        instance.setValue(nominalAttribute, "B");
+        Date dateValue = new Date();
+        instance.setValue(dateAttribute, dateValue.getTime());
+        //Case 1
+        Assertions.assertThat(Utils.formatValue(instance, numericAttribute)).isEqualTo(
+                String.valueOf(instance.value(numericAttribute)));
+        //Case 2
+        Assertions.assertThat(Utils.formatValue(instance, dateAttribute)).isEqualTo(
+                String.format(STRING_VALUE_FORMAT, instance.stringValue(dateAttribute)));
+        //Case 3
+        Assertions.assertThat(Utils.formatValue(instance, nominalAttribute)).isEqualTo(
+                String.format(STRING_VALUE_FORMAT, instance.stringValue(nominalAttribute)));
     }
 }
