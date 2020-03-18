948604634425fbb65b42aef15c3ba73b6a443014
Roman93 <bat1238@yandex.ru>
2020-03-19 01:25:47 +0700

    Support templates including for velocity

--- eca-gui/src/main/java/eca/config/VelocityConfigService.java
+++ eca-gui/src/main/java/eca/config/VelocityConfigService.java
@@ -3,6 +3,7 @@ package eca.config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.velocity.Template;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.app.event.implement.IncludeRelativePath;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
@@ -26,6 +27,7 @@ public class VelocityConfigService {
             velocityEngine = new VelocityEngine();
             velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, CLASSPATH_PROPERTY);
             velocityEngine.setProperty(CLASSPATH_RESOURCE_LOADER_PROPERTY, ClasspathResourceLoader.class.getName());
+            velocityEngine.setProperty(RuntimeConstants.EVENTHANDLER_INCLUDE, IncludeRelativePath.class.getName());
             velocityEngine.init();
         } catch (Exception ex) {
             log.error("Error in init velocity engine: ", ex);
