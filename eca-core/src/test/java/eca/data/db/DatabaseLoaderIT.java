5aa880d3b783d4fd60e1026d638b137a310c3dc1
Батыгин Роман Игоревич <batygin@self.team>
2020-10-14 11:43:19 +0700

    Implements db integration tests

--- eca-core/src/test/java/eca/data/db/DatabaseLoaderIT.java
+++ eca-core/src/test/java/eca/data/db/DatabaseLoaderIT.java
@@ -5,10 +5,9 @@ import eca.config.DbTestCase;
 import eca.config.EcaCoreTestConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import weka.core.InstanceComparator;
 import weka.core.Instances;
 
-import java.util.List;
+import java.sql.SQLException;
 import java.util.Map;
 
 import static eca.TestHelperUtils.loadInstances;
@@ -25,6 +24,8 @@ class DatabaseLoaderIT {
 
     private final JdbcQueryExecutor jdbcQueryExecutor = new JdbcQueryExecutor();
 
+    private final ConnectionDescriptorBuilder connectionDescriptorBuilder = new ConnectionDescriptorBuilder();
+
     private Map<DataBaseType, DatabaseTestConfig> databaseTestConfigMap;
 
     @BeforeEach
@@ -37,49 +38,53 @@ class DatabaseLoaderIT {
         databaseTestConfigMap.forEach((key, value) -> {
             ConnectionDescriptor connectionDescriptor = createConnectionDescriptor(key, value);
             jdbcQueryExecutor.setConnectionDescriptor(connectionDescriptor);
-            performDatabaseTests(value.getTestCases());
+            try {
+                jdbcQueryExecutor.open();
+                performDatabaseTests(value);
+            } catch (Exception ex) {
+                fail(ex);
+            } finally {
+                close();
+            }
         });
     }
 
-    private void performDatabaseTests(List<DbTestCase> dbTestCases) {
+    private void performDatabaseTests(DatabaseTestConfig databaseTestConfig) {
         try {
-            for (DbTestCase dbTestCase : dbTestCases) {
-                jdbcQueryExecutor.setSource(dbTestCase.getSqlQuery());
-                Instances actual = jdbcQueryExecutor.loadInstances();
-                Instances expected = loadInstances(dbTestCase.getExpectedDataFile());
+            for (DbTestCase dbTestCase : databaseTestConfig.getTestCases()) {
+                executeQuery(dbTestCase);
             }
-        } catch (Exception ex) {
+         } catch (Exception ex) {
             fail(ex);
         }
     }
 
+    private void executeQuery(DbTestCase dbTestCase) throws Exception {
+        jdbcQueryExecutor.setSource(dbTestCase.getSqlQuery());
+        Instances actual = jdbcQueryExecutor.loadInstances();
+        Instances expected = loadInstances(dbTestCase.getExpectedDataFile());
+        System.out.println(actual.equalHeadersMsg(expected));
+    }
+
     private ConnectionDescriptor createConnectionDescriptor(DataBaseType dataBaseType,
                                                             DatabaseTestConfig databaseTestConfig) {
-        return dataBaseType.handle(new DataBaseTypeVisitor<ConnectionDescriptor>() {
-            @Override
-            public ConnectionDescriptor caseMySql() {
-                return null;
-            }
+        ConnectionDescriptor connectionDescriptor = dataBaseType.handle(connectionDescriptorBuilder);
+        connectionDescriptor.setDriver(databaseTestConfig.getDriver());
+        connectionDescriptor.setDataBaseName(databaseTestConfig.getDataBaseName());
+        connectionDescriptor.setHost(databaseTestConfig.getHost());
+        connectionDescriptor.setLogin(databaseTestConfig.getLogin());
+        connectionDescriptor.setPassword(databaseTestConfig.getPassword());
+        if (databaseTestConfig.getPort() != null) {
+            connectionDescriptor.setPort(databaseTestConfig.getPort());
+        }
+        return connectionDescriptor;
+    }
 
-            @Override
-            public ConnectionDescriptor casePostgreSQL() {
-                return null;
-            }
-
-            @Override
-            public ConnectionDescriptor caseMSAccess() {
-                return null;
-            }
-
-            @Override
-            public ConnectionDescriptor caseMSSQL() {
-                return null;
-            }
-
-            @Override
-            public ConnectionDescriptor caseSQLite() {
-                return null;
-            }
-        });
+    private void close() {
+        try {
+            jdbcQueryExecutor.close();
+        } catch (SQLException e) {
+            fail(e);
+        }
     }
 }
