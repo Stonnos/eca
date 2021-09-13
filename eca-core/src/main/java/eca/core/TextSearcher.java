a21cc133d9233245de3a79af106ac546263b8c1e
Батыгин Роман Игоревич <batygin@self.team>
2021-09-13 14:37:50 +0700

    #ECA-30 - javadoc fix

--- eca-core/src/main/java/eca/core/TextSearcher.java
+++ eca-core/src/main/java/eca/core/TextSearcher.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Objects;
 
 /**
- * Case sensitive text searcher.
+ * Case insensitive text searcher.
  *
  * @author Roman Batygin
  */
