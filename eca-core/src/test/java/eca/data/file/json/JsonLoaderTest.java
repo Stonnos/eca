93ad98f45ce2184abdee9b3490beed7a6b010ad1
Roman <bat1238@yandex.ru>
2023-05-18 21:31:23 +0700

    #ECA-40 - unit tests for loading data with invalid class name

--- eca-core/src/test/java/eca/data/file/json/JsonLoaderTest.java
+++ eca-core/src/test/java/eca/data/file/json/JsonLoaderTest.java
@@ -3,6 +3,8 @@ package eca.data.file.json;
 import eca.data.file.BaseFileLoaderTest;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Unit tests for {@link JsonLoader} class.
  *
@@ -11,11 +13,17 @@ import org.junit.jupiter.api.Test;
 class JsonLoaderTest extends BaseFileLoaderTest {
 
     private static final String DATA_CREDIT_JSON = "data/credit.json";
+    private static final String DATA_IRIS_WITH_INVALID_CLASS_JSON = "data/iris-with-invalid-class.json";
 
     private final JsonLoader jsonLoader = new JsonLoader();
 
     @Test
     void testLoadDataFromJsonFile() {
         executeTest(jsonLoader, DATA_CREDIT_JSON);
+    }
+
+    @Test
+    void testLoadDataWithInvalidClassName() {
+        assertThrows(IllegalStateException.class, () -> executeTest(jsonLoader, DATA_IRIS_WITH_INVALID_CLASS_JSON));
     }
 }
