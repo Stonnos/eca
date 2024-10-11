d1673cdbacfc0d0aa5da3ac4e9a72dcd0eb4939e
Roman <bat1238@yandex.ru>
2024-10-12 03:28:13 +0700

    ECS-47 added some unit tests

--- eca-core/src/test/java/eca/data/file/xls/XlsLoaderTest.java
+++ eca-core/src/test/java/eca/data/file/xls/XlsLoaderTest.java
@@ -3,6 +3,8 @@ package eca.data.file.xls;
 import eca.data.file.BaseFileLoaderTest;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Unit tests for {@link XLSLoader} class.
  *
@@ -10,11 +12,33 @@ import org.junit.jupiter.api.Test;
  */
 class XlsLoaderTest extends BaseFileLoaderTest {
     private static final String DATA_CREDIT_XLSX = "data/credit.xlsx";
+    private static final String INVALID_DATA_DIFFERENT_CELL_TYPES_XLSX = "data/invalid_data_different_cell_types.xlsx";
+
+    private static final String INVALID_DATA_EMPTY_HEADER_XLSX = "data/invalid_data_empty_header.xlsx";
+    private static final String INVALID_DATA_CELLS_SIZE_XLSX = "data/invalid_data_cells_size.xlsx";
 
     private final XLSLoader xlsLoader = new XLSLoader();
 
     @Test
     void testLoadDataFromXlsxFile() {
         executeTest(xlsLoader, DATA_CREDIT_XLSX);
+    }
+
+    @Test
+    void testLoadDataFromXlsxFileWithDifferentCellTypesShouldThrowError() {
+        assertThrows(IllegalArgumentException.class,
+                () -> executeTest(xlsLoader, INVALID_DATA_DIFFERENT_CELL_TYPES_XLSX));
+    }
+
+    @Test
+    void testLoadDataFromXlsxFileWithEmptyHeaderShouldThrowError() {
+        assertThrows(IllegalArgumentException.class,
+                () -> executeTest(xlsLoader, INVALID_DATA_EMPTY_HEADER_XLSX));
+    }
+
+    @Test
+    void testLoadDataFromXlsxFileWithInvalidCellsSizeShouldThrowError() {
+        assertThrows(IllegalArgumentException.class,
+                () -> executeTest(xlsLoader, INVALID_DATA_CELLS_SIZE_XLSX));
     }
 }
